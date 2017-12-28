56a4b87d7bed2184689c4df939ac85b8a0d8f487
edsonsalvador30 <edsonsalvador30>
2017-12-28 19:07:39 +0000

    ter

---
+++ src/main/java/app/entity/NomeDaClasse.java
@@ -0,0 +1,21 @@
+package app.entity;
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author edson.fonseca@techne.com.br
+ * @version 1.0
+ * @since 2017-12-28
+ *
+ */
+ 
+public class NomeDaClasse {
+
+	/**
+	 * Construtor
+	 **/
+	public NomeDaClasse (){
+	}
+
+}
